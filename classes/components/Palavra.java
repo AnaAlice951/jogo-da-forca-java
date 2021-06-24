b4f1e7e8e36cbaf8f213e9bc77405725f9303385
ArthurSouzaC <arthursouza.info@gmail.com>
2021-06-24 09:24:17 -0300

    first release

---
+++ classes/components/Palavra.java
@@ -0,0 +1,9 @@
+package classes.components;
+
+// Componente gráfico da biblioteca Swing
+import javax.swing.JLabel;
+
+/**
+ * Classe responsável por renderizar a palavra escondida
+ */
+public class Palavra extends JLabel{}
